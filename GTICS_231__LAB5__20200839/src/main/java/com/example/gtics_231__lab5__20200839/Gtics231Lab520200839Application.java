95fa629d8adbc5a1f25cf1309ec51c7918993e24
DiegoLG15 <a20200839@pucp.edu.pe>
2023-04-25 20:35:23 -0500

    first commit

---
+++ GTICS_231__LAB5__20200839/src/main/java/com/example/gtics_231__lab5__20200839/Gtics231Lab520200839Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_231__lab5__20200839;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gtics231Lab520200839Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gtics231Lab520200839Application.class, args);
+	}
+
+}
---
+++ GTICS_231__LAB5__20200839/src/test/java/com/example/gtics_231__lab5__20200839/Gtics231Lab520200839ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_231__lab5__20200839;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gtics231Lab520200839ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
